0f72ee885f4f986411e05b102b4f30804d39b1f6
d0vesei <31807036+d0vesei@users.noreply.github.com>
2024-01-12 23:40:45 +0100

    Update export.php

--- export.php
+++ export.php
@@ -1,7 +1,7 @@
 <?php
 
 $con=mysqli_init(); 
-mysqli_ssl_set($con, 'localhost','root','aib', 'microblogging'); 
+mysqli_ssl_set($con, 'localhost','','', ''); 
 mysqli_real_connect($con, "notify.mysql.database.azure.com", "", "", "", 3306);
 
 ?>
